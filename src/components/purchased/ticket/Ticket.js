1938be1562479c8fab79fe13ee43d464f9651088
Edidiong Obodom <edijay17@gmail.com>
2022-12-20 02:10:27 +0100

    modified:   src/components/purchased/ticket/Ticket.module.css

--- src/components/purchased/ticket/Ticket.js
+++ src/components/purchased/ticket/Ticket.js
@@ -6,22 +6,51 @@ const Ticket = () => {
 
   const handleBack = () => {
     return navigate("/");
-  }
+  };
 
-    return (
-        <div className="container">
-            <div className="container">
-            </div>
-            <div className={`container grid-container ${classes.options}`}>
-            <div className={`${classes.optionsIcon}`} onClick={handleBack}>
-            <i className="fa-solid fa-arrow-left"></i>
-            </div>
-            <div className={`grid-item ${classes.ID}`}>
-            <h3>#04512365741</h3>
-            </div>
-            </div>
+  return (
+    <div className="container">
+      <div className={`container grid-container ${classes.options}`}>
+        <div className={`${classes.optionsIcon}`} onClick={handleBack}>
+          <i className="fa-solid fa-arrow-left"></i>
         </div>
-    );
-}
+        <div className={`grid-item ${classes.ID}`}>
+          <h3>#04512365741</h3>
+        </div>
+      </div>
+      <div className="container">
+        <div
+          className={`container ${classes.img}`}
+          style={{
+            backgroundImage: `url(https://media.istockphoto.com/id/1412446063/photo/multiracial-smiling-friends-enjoying-on-the-boat.jpg?s=612x612&w=is&k=20&c=SSIv0T1DBO9z60EnM_FhVFVUvkXP4vWj54793--xGLY=)`,
+            // backgroundImage: `url(${externalImage})`,
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center",
+            height: "250px",
+            width: "100%",
+          }}
+        ></div>
+      </div>
+      <div className="container">
+        <div className="container">
+          <div className={`container hstack ${classes.info}`}>
+          <div>
+          <h3>Calabar Pool Party</h3>
+          </div>
+          <div className={`${classes.date}`}>
+              <p className={`${classes.go}`}>Eleven Eleven, Calabar</p>
+              <p className={`${classes.p2}`}>Marina Resort</p>
+            </div>
+          <div className={`ms-auto ${classes.date}`}>
+              <p className={`${classes.go}`}>Eleven Eleven, Calabar</p>
+              <p className={`${classes.p2}`}>Marina Resort</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Ticket;
