59c153c8c48b916abf83058afbf533c88d811858
DinnoCreator <edijay17@gmail.com>
2023-01-20 12:30:20 +0100

    modified:   src/components/signup/SignUpView.js

--- src/components/login/LoginView.js
+++ src/components/login/LoginView.js
@@ -8,39 +8,39 @@ const LoginView = () => {
         <h3>Never miss the fun...</h3>
         <div className="container">
           <form className="container">
-            <div class="mb-3">
-              <label for="exampleInputEmail1" class="form-label">
+            <div className="mb-3">
+              <label htmlFor="exampleInputEmail1" className="form-label">
                 Email address
               </label>
               <input
                 type="email"
-                class="form-control"
+                className="form-control"
                 id="exampleInputEmail1"
                 aria-describedby="emailHelp"
               />
             </div>
-            <div class="mb-3">
-              <label for="exampleInputPassword1" class="form-label">
+            <div className="mb-3">
+              <label htmlFor="exampleInputPassword1" className="form-label">
                 Password
               </label>
               <span className={`d-flex ${classes.white}`}>
               <input
                 type="password"
-                class="form-control me-2"
+                className="form-control me-2"
                 id="exampleInputPassword1"
               />
               <button className={`btn ${classes.login}`} type="submit">
-              <i class="fa-regular fa-eye-slash"></i>
+              <i className="fa-regular fa-eye-slash"></i>
               </button>
               </span>
             </div><br/>
-            <div class={`d-grid gap-2 btn ${classes.login}`}>
+            <div className={`d-grid gap-2 btn ${classes.login}`}>
               Login
             </div>
 
             <hr/>
             <h4 className="center mb-3">or</h4>
-            <div class={`d-grid gap-2 btn ${classes.signup}`}>
+            <div className={`d-grid gap-2 btn ${classes.signup}`}>
               Signup
             </div>
           </form>
--- src/components/signup/SignUpView.js
+++ src/components/signup/SignUpView.js
@@ -8,46 +8,46 @@ const SignUpView = () => {
         <h3>Never miss the fun...</h3>
         <div className="container">
           <form className="container">
-            <div class="mb-3">
-              <label for="exampleInputEmail1" class="form-label">
+            <div className="mb-3">
+              <label htmlFor="exampleInputEmail1" className="form-label">
                 Email address
               </label>
               <input
                 type="email"
-                class="form-control"
+                className="form-control"
                 id="exampleInputEmail1"
                 aria-describedby="emailHelp"
               />
             </div>
-            <div class="mb-3">
-              <label for="labfname" class="form-label">
+            <div className="mb-3">
+              <label htmlFor="labfname" className="form-label">
                 First name
               </label>
-              <input type="text" class="form-control" id="labfname" />
+              <input type="text" className="form-control" id="labfname" />
             </div>
-            <div class="mb-3">
-              <label for="lablname" class="form-label">
+            <div className="mb-3">
+              <label htmlFor="lablname" className="form-label">
                 Last name
               </label>
-              <input type="text" class="form-control" id="lablname" />
+              <input type="text" className="form-control" id="lablname" />
             </div>
-            <div class="mb-3">
-              <label for="exampleInputPassword1" class="form-label">
+            <div className="mb-3">
+              <label htmlFor="exampleInputPassword1" className="form-label">
                 Password
               </label>
               <span className={`d-flex ${classes.white}`}>
                 <input
                   type="password"
-                  class="form-control me-2"
+                  className="form-control me-2"
                   id="exampleInputPassword1"
                 />
                 <button className={`btn ${classes.login}`} type="submit">
-                  <i class="fa-regular fa-eye-slash"></i>
+                  <i className="fa-regular fa-eye-slash"></i>
                 </button>
               </span>
             </div>
             <br />
-            <div class={`d-grid gap-2 btn ${classes.login}`}>Signup</div>
+            <div className={`d-grid gap-2 btn ${classes.login}`}>Signup</div>
 
             <hr />
           </form>
