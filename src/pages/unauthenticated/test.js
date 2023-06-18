d16d171c78c2b8f42606e0f34a207780f7f8fa75
Edidiong Obodom <edijay17@gmail.com>
2023-06-18 22:00:57 +0100

    modified:   src/pages/unauthenticated/test.js

--- src/pages/unauthenticated/test.js
+++ src/pages/unauthenticated/test.js
@@ -5,36 +5,51 @@ const Test = () => {
   const [acc, setAcc] = useState("");
   const [lat, setLat] = useState("");
   const [long, setLong] = useState("");
-  const [alt, setAlt] = useState("");
+  const [width, setAlt] = useState("");
   const [city, setCity] = useState("");
   const [state, setState] = useState("");
 
   const geoLocator = () => {
-    navigator.geolocation.watchPosition(async(position) => {
-      const { accuracy, latitude, longitude, altitude } = position.coords;
+    function widthResizer() {
+      let width = window.innerWidth;
+      return setAlt(width);
+    }
+    widthResizer();
+
+    // Getting the width of the browser whenever the screen resolution changes.
+    window.addEventListener("resize", widthResizer);
+    navigator.geolocation.watchPosition(async (position) => {
+      const { accuracy, latitude, longitude } = position.coords;
       // Show a map centered at latitude / longitude.
       setLat(latitude);
       setLong(longitude);
-      setAlt(altitude);
       setAcc(accuracy);
-        let res = await axios.get(
-            `https://api.weatherapi.com/v1/forecast.json?key=ce79536048da40f0a06202113231806&q=${latitude},${longitude}&days=1&aqi=no&alerts=no`
-        );
-        console.log(res.data);
-        setState(res.data.location.region);
-       return  setCity(res.data.location.name);
+
+      let res = await axios.get(
+        `https://api.weatherapi.com/v1/forecast.json?key=ce79536048da40f0a06202113231806&q=${latitude},${longitude}&days=1&aqi=no&alerts=no`
+      );
+      console.log(res.data);
+      setState(res.data.location.region);
+      return setCity(res.data.location.name);
     });
   };
   return (
     <div>
-      <h1 className="stuff" onClick={geoLocator}>
-        Test
-      </h1>
-      <h3>Accuracy: {acc}</h3>
-      <h3>Latitude: {lat}</h3>
-      <h3>Longitude: {long}</h3>
-      <h3>City: {city}</h3>
-      <h3>State: {state}</h3>
+      {width === "" || Number(width) > 750 ? (
+        <h1 onClick={geoLocator}>please use a phone</h1>
+      ) : (
+        <>
+          <h1 className="stuff" onClick={geoLocator}>
+            Test
+          </h1>
+          <h3>Accuracy: {acc}</h3>
+          <h3>Latitude: {lat}</h3>
+          <h3>Longitude: {long}</h3>
+          <h3>City: {city}</h3>
+          <h3>State: {state}</h3>
+          <h3>Screen width: {width}</h3>
+        </>
+      )}
     </div>
   );
 };
