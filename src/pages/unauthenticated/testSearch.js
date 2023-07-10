ccee8b67f665488da8e864ca91057dd065c2875c
Edidiong Obodom <edijay17@gmail.com>
2023-07-10 18:35:14 +0100

    new file:   src/components/Html5QrcodePlugin/Html5QrcodePlugin.js

--- src/pages/unauthenticated/testSearch.js
+++ src/pages/unauthenticated/testSearch.js
@@ -1,36 +1,24 @@
-import {Html5QrcodeScanner} from "html5-qrcode";
-import {useState} from "react";
+// import {Html5QrcodeScanner} from "html5-qrcode";
+import { useState } from "react";
+import Html5QrcodePlugin from "../../components/Html5QrcodePlugin/Html5QrcodePlugin";
 
 const ScanAll = () => {
-  const {text, setText} = useState("Nothing to show");
-  function onScanSuccess(decodedText, decodedResult) {
-    // handle the scanned code as you like, for example:
-    console.log(`Code matched = ${decodedText}`, decodedResult);
+  const [text, setText] = useState("Please scan a qrcode");
+  const onNewScanResult = (decodedText, decodedResult) => {
     return setText(decodedText);
-  }
-
-  function onScanFailure(error) {
-    // handle scan failure, usually better to ignore and keep scanning.
-    // for example:
-    console.warn(`Code scan error = ${error}`);
-    return setText(error);
-  }
-
-  let html5QrcodeScanner = new Html5QrcodeScanner(
-    "reader",
-    { fps: 10, qrbox: { width: 250, height: 250 } },
-    /* verbose= */ false
-  );
-
-  html5QrcodeScanner.render(onScanSuccess, onScanFailure)
+  };
 
   return (
     <>
-    <div id="reader" width="600px"> 1234</div>
-    <div><h1 className="mt-3">{text}</h1></div>
+      <Html5QrcodePlugin
+        fps={10}
+        qrbox={250}
+        disableFlip={false}
+        qrCodeSuccessCallback={onNewScanResult}
+      />
+      <h1 className="container mt-3">{text}</h1>
     </>
   );
 };
-
 
 export default ScanAll;
